05fb046147593adc73b0e96ce2fedebaa258d68f
xuekaixin <492299478@qq.com>
2018-06-08 08:40:08 +0800

    2018-06-08

---
+++ 4-input_and_while/imput.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+#
+# **********************************************
+# Author: xuekaixin
+# Mail: jinweiayy@gmail.com
+# Website: www.xuejinwei.com
+# Date: 2018-06-07 21:55
+# Filename: imput.py
+# Description: 
+# Applicable:
+# **********************************************
+#
+variable = input("请输入你的名字：")
+print(variable)
