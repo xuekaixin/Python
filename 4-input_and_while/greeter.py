2ade2023489ebb3c90de60125c5620c6678f591e
xuekaixin <492299478@qq.com>
2018-06-20 23:00:41 +0800

    2018-06-20

---
+++ 4-input_and_while/greeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*
+#
+# **********************************************
+# Author: xuekaixin
+# Mail: jinweiayy@gmail.com
+# Website: www.xuejinwei.com
+# Date: 2018-06-20 22:57
+# Filename: greeter.py
+# Description: 
+# Applicable:
+# **********************************************
+#
+string = "Hello World!"
+string += "\ncat!"
+
+name = input(string)
+print(name)
